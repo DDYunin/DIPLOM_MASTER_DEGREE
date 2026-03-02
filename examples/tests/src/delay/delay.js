eb33b2e2c1efd18167de73a3ebcd7207cb1aaf6a
Daniil Yunin <daniil.yunin@alarislabs.com>
2026-03-02 08:34:28 +0300

    init: reset project

--- examples/tests/src/delay/delay.js
+++
@@ -1,9 +0,0 @@
-function delay(callback, ms) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(callback());
-    }, ms)
-  });
-}
-
-module.exports = delay;
--- examples/tests/src/delay/delay.test.js
+++
@@ -1,8 +0,0 @@
-const delay = require('./delay');
-
-describe('Delay', () => {
-  test('should return the number of delays', async () => {
-    const result = await delay(() => 5 + 5, 1000);
-    expect(result).toBe(10);
-  })
-})
